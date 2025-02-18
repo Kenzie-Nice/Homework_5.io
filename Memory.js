51b81e6c0e7cc95cc9c696e6fec1bbd54a574ad7
Kenzie-Nice <157401036+Kenzie-Nice@users.noreply.github.com>
2025-02-18 15:58:46 -0700

    Update Memory.js

--- Memory.js
+++ Memory.js
@@ -1,10 +1,10 @@
 const images = [
-    'images/Green_tea_Kukicha.jpg', 'images/Green_tea_Kukicha.jpg', 
-    'images/coffee-mug_NVKXLIKJ25.jpg', 'images/coffee-mug_NVKXLIKJ25.jpg', 
-    'images/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 'images/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 
-    'images/pexels-photo-6711567.jpeg', 'images/pexels-photo-6711567.jpeg', 
-    'images/tea-black-tea-drink-tea-cup-preview.jpg', 'images/tea-black-tea-drink-tea-cup-preview.jpg', 
-    'images/tea-candy-green-tea-black-tea.jpg', 'images/tea-candy-green-tea-black-tea.jpg'
+    'Homework_5.io/images/Green_tea_Kukicha.jpg', 'Homework_5.io/images/Green_tea_Kukicha.jpg', 
+    'Homework_5.io/images/coffee-mug_NVKXLIKJ25.jpg', 'Homework_5.io/images/coffee-mug_NVKXLIKJ25.jpg', 
+    'Homework_5.io/images/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 'Homework_5.io/images/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 
+    'Homework_5.io/images/pexels-photo-6711567.jpeg', 'Homework_5.io/images/pexels-photo-6711567.jpeg', 
+    'Homework_5.io/images/tea-black-tea-drink-tea-cup-preview.jpg', 'Homework_5.io/images/tea-black-tea-drink-tea-cup-preview.jpg', 
+    'Homework_5.io/images/tea-candy-green-tea-black-tea.jpg', 'Homework_5.io/images/tea-candy-green-tea-black-tea.jpg'
 ];
 
 images.sort(() => Math.random() - 0.5);
@@ -16,11 +16,11 @@ let lockBoard = false;
 
 images.forEach((image, index) => {
     let img = document.createElement('img');
-    img.src = 'images/blank.jpg';
+    img.src = 'Homework_5.io/images/blank.jpg';
     img.dataset.index = index;
     
     img.onclick = () => {
-        if (lockBoard || img.src !== `${window.location.origin}/images/blank.jpg`) return;
+        if (lockBoard || img.src !== `${window.location.origin}/Homework_5.io/images/blank.jpg`) return;
 
         img.src = image;
 
@@ -42,8 +42,8 @@ function checkMatch() {
     } else {
         console.log("Not a match, flipping back.");
         setTimeout(() => {
-            firstPick.img.src = 'images/blank.jpg';
-            secondPick.img.src = 'images/blank.jpg';
+            firstPick.img.src = 'Homework_5.io/images/blank.jpg';
+            secondPick.img.src = 'Homework_5.io/images/blank.jpg';
         }, 500);
     }
 
