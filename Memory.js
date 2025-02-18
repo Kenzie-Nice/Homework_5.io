a79d0a0fdf141b09d324947417fbf4e2b1777f59
Kenzie-Nice <157401036+Kenzie-Nice@users.noreply.github.com>
2025-02-18 16:06:22 -0700

    Update Memory.js

--- Memory.js
+++ Memory.js
@@ -1,34 +1,34 @@
 const images = [
-    'Homework_5.io/images/Green_tea_Kukicha.jpg', 'Homework_5.io/images/Green_tea_Kukicha.jpg', 
-    'Homework_5.io/images/coffee-mug_NVKXLIKJ25.jpg', 'Homework_5.io/images/coffee-mug_NVKXLIKJ25.jpg', 
-    'Homework_5.io/images/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 'Homework_5.io/images/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 
-    'Homework_5.io/images/pexels-photo-6711567.jpeg', 'Homework_5.io/images/pexels-photo-6711567.jpeg', 
-    'Homework_5.io/images/tea-black-tea-drink-tea-cup-preview.jpg', 'Homework_5.io/images/tea-black-tea-drink-tea-cup-preview.jpg', 
-    'Homework_5.io/images/tea-candy-green-tea-black-tea.jpg', 'Homework_5.io/images/tea-candy-green-tea-black-tea.jpg'
+    'images/here/Green_tea_Kukicha.jpg', 'images/here/Green_tea_Kukicha.jpg', 
+    'images/here/coffee-mug_NVKXLIKJ25.jpg', 'images/here/coffee-mug_NVKXLIKJ25.jpg', 
+    'images/here/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 'images/here/main-qimg-14ff668b1c08bfab4cc710cdfba6a9d3-lq.jpg', 
+    'images/here/pexels-photo-6711567.jpeg', 'images/here/pexels-photo-6711567.jpeg', 
+    'images/here/tea-black-tea-drink-tea-cup-preview.jpg', 'images/here/tea-black-tea-drink-tea-cup-preview.jpg', 
+    'images/here/tea-candy-green-tea-black-tea.jpg', 'images/here/tea-candy-green-tea-black-tea.jpg'
 ];
 
-images.sort(() => Math.random() - 0.5);
+images.sort(() => Math.random() - 0.5); // Shuffle images
 
 const grid = document.getElementById('grid');
 let firstPick = null;
 let secondPick = null;
-let lockBoard = false;
+let lockBoard = false; // Prevents clicking during match check
 
 images.forEach((image, index) => {
     let img = document.createElement('img');
-    img.src = 'Homework_5.io/images/blank.jpg';
+    img.src = 'images/here/blank.jpg'; // Default blank image
     img.dataset.index = index;
     
     img.onclick = () => {
-        if (lockBoard || img.src !== `${window.location.origin}/Homework_5.io/images/blank.jpg`) return;
+        if (lockBoard || img.src !== window.location.origin + '/images/here/blank.jpg') return; // Prevent clicking revealed images
 
-        img.src = image;
+        img.src = images[index]; // Reveal image
 
         if (!firstPick) {
             firstPick = { img, index };
         } else {
             secondPick = { img, index };
-            lockBoard = true;
+            lockBoard = true; // Prevent more clicks
             setTimeout(checkMatch, 1000);
         }
     };
@@ -42,8 +42,8 @@ function checkMatch() {
     } else {
         console.log("Not a match, flipping back.");
         setTimeout(() => {
-            firstPick.img.src = 'Homework_5.io/images/blank.jpg';
-            secondPick.img.src = 'Homework_5.io/images/blank.jpg';
+            firstPick.img.src = 'images/here/blank.jpg'; 
+            secondPick.img.src = 'images/here/blank.jpg';
         }, 500);
     }
 
